dc1b7986000d67f129af9ace85cadfad5ff3ca65
jucasantos <juliadecarvalhosantos40@gmail.com>
2023-12-11 22:04:41 +0000

    Buscar página html e renderizar no elemento root #1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const renderPage = (pageName: string): void => {
+    fetch(`pages/${pageName}.html`)
+    .then((resp) => resp.text())
+    .then((html) => {
+        const rootElem = document.getElementById("root") as HTMLElement;
+        
+        rootElem.innerHTML = html; 
+}); 
+};
+
+renderPage("quiz");

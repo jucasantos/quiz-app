664e074e6c70aa222c634d87e1060fcb6bbbcdf5
jucasantos <juliadecarvalhosantos40@gmail.com>
2023-12-15 21:48:26 +0000

    Capturar o nome do utilizador no input e armazenar no local storage #3

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,30 @@
 const renderPage = (pageName: string): void => {
-    fetch(`pages/${pageName}.html`)
+  fetch(`pages/${pageName}.html`)
     .then((resp) => resp.text())
     .then((html) => {
-        const rootElem = document.getElementById("root") as HTMLElement;
-        
-        rootElem.innerHTML = html; 
+      const rootElem = document.getElementById("root") as HTMLElement;
 
-        const form = document.getElementById("form-start") as HTMLFormElement;
-        
-        form.addEventListener("submit", handleLogIn);
-}); 
+      rootElem.innerHTML = html; //buscar o html da pag start
+
+      const handleLogIn = (event: Event): void => {
+        //evento submit
+        event.preventDefault();
+
+        const inputName = document.getElementById(
+          "input-name"
+        ) as HTMLInputElement;
+
+        let valueName = inputName.value; //valor do input
+
+        localStorage.setItem("Name", valueName);
+
+        console.log(valueName);
+      };
+
+      const form = document.getElementById("form-start") as HTMLFormElement;
+
+      form.addEventListener("submit", handleLogIn);
+    });
 };
 
 renderPage("start");
-
-const handleLogIn = (event : Event ): void => {
-    event.preventDefault();
-    
-    const inputName = document.getElementById("input-name") as HTMLInputElement; 
-    
-    let valueName= inputName.value ; 
-
-    localStorage.setItem("Name", valueName);
-    
-    console.log(valueName);
-
-}
-
